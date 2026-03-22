3cacad2903e70c0ba2483427ce020a6c02c69ec2
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-22 16:36:18 -0300

    Crear auth.py

---
+++ backend/auth.py
@@ -0,0 +1,16 @@
+from database import connect
+from security import verify
+
+def login(password):
+    conn = connect()
+    c = conn.cursor()
+
+    c.execute("SELECT name, password FROM users")
+    users = c.fetchall()
+
+    for user in users:
+        name, hashed = user
+        if verify(password, hashed):
+            return name
+
+    return None
